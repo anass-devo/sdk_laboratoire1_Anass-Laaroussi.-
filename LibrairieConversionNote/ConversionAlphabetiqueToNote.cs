21c14b143e5d6ca8a4b122975a0c068532142155
anass-devo <anasslaaroussi01@gmail.com>
2023-06-13 22:52:34 -0500

    creation de la methode transformer

---
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public static string Transformer(string lettre)
+        {
+            string note;
+
+            switch (lettre.ToUpper())
+            {
+                case "A":
+                    note = "LA";
+                    break;
+                case "B":
+                    note = "SI";
+                    break;
+                case "C":
+                    note = "DO";
+                    break;
+                case "D":
+                    note = "RÉ";
+                    break;
+                case "E":
+                    note = "MI";
+                    break;
+                case "F":
+                    note = "FA";
+                    break;
+                case "G":
+                    note = "SOL";
+                    break;
+                default:
+                    note = "";
+                    break;
+            }
+
+            return note;
+        }
+    }
+}
---
+++ SDK_Laboratoire1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using LibrairieConversionNote;
+
+namespace SDK_Laboratoire1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
