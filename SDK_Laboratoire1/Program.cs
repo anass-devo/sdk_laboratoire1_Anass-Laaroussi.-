815ac87c7e777cbf4ee96730216b160f8ebc932a
anass-devo <anasslaaroussi01@gmail.com>
2023-06-16 15:30:26 -0500

    la derniere MAJ

--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -7,37 +7,61 @@ namespace SDK_Laboratoire1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)  // fonction principale de programme
         {
             string lettre;
             string note;
 
-            Console.CancelKeyPress += (sender, e) =>
+            Console.CancelKeyPress += (sender, e) =>  // fermiture du programme
             {
 
-                Console.WriteLine("Vous avez quitté le programme.");
+                Console.WriteLine("Vous avez quitté le programme."); 
                 Environment.Exit(0);
 
             };
-            List<string> notesValides = new List<string> { "A", "B", "C", "D", "E", "F", "G" };
+            List<string> notesValides = new List<string> { "A", "B", "C", "D", "E", "F", "G" }; // liste des valeur pou rle controle de saisie
             while (true)
-            {
-                Console.WriteLine("Entrez une lettre alphabétique (A, B, C, D, E, F, G) Pour quitter appuyer sur Ctrl+c :");
-                lettre = Console.ReadLine();
-                lettre = lettre.ToUpper();
+            { // controle de choix de type de conversion .
+                Console.WriteLine("Quel type de conversion souhaitez-vous faire ?");
+                Console.WriteLine("1. Convertir de l'alphabet à la note");
+                Console.WriteLine("2. Convertir de la note à l'alphabet");
+                Console.WriteLine("Appuyez sur Ctrl+C pour quitter.");
 
-                if (string.IsNullOrEmpty(lettre))
-                    continue;
+                string choix = Console.ReadLine();
 
-                note = ConversionAlphabetiqueToNote.Transformer(lettre);
-                if (!notesValides.Contains(lettre))
+                if (choix == "1")
                 {
-                    Console.WriteLine("La note n'existe pas.");
+                    while (true)  // execution mode 1 
+                    {
+                        Console.WriteLine("Entrez une lettre alphabétique (A, B, C, D, E, F, G) Pour quitter appuyer sur Ctrl+c :");
+                        lettre = Console.ReadLine();
+                        lettre = lettre.ToUpper();
+
+                        if (string.IsNullOrEmpty(lettre))
+                            continue;
+
+                        note = ConversionAlphabetiqueToNote.Transformer(lettre);
+                        if (!notesValides.Contains(lettre))
+                        {
+                            Console.WriteLine("La note n'existe pas.");
+                            continue;
+                        }
+
+                        Console.WriteLine($"La note qui correspond a la Lettre ' {lettre} ' est : {note} ");
+                    }
+                }
+                else if (choix == "2")
+                {
+                    // fonction pas demander pour ce lab1
+                    Console.WriteLine("Ce mode sera disponible bientot !!!!!!!!");
                     continue;
                 }
-
-                Console.WriteLine($"La note qui correspond a la Lettre ' {lettre} ' est : {note} ");
+                else
+                {
+                    Console.WriteLine("Choix invalide. Veuillez réessayer.");
+                }
             }
+           
 
         }
     }
